c62d2fab2e75dc92538657f7bc9b778873f7e97b
sunmyoung <roeowo123@naver.com>
2021-06-21 23:57:55 +0900

    onChange 이벤트 핸들러

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-//Create Content
+//Update Content
 class CreateContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.data.title,
+            desc: this.props.data.desc
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e) {
+        this.setState = ({
+            //자바스크립트 최신 문법
+            [e.target.name]: e.target.value
+        });
+    }
     render() {
-        console.log(this.props.data);
         return (
             <div>
                 <h1>Update</h1>
@@ -13,8 +26,19 @@ class CreateContent extends Component {
                         e.target.desc.value
                     );
                 }.bind(this)}>
-                    <input type="text" name="title" placeholder='title'></input>
-                    <textarea name="desc" placeholder="desc"></textarea>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder='title'
+                        value={this.state.title}
+                        onChange={this.inputFormHandler}
+                    ></input>
+                    <textarea
+                        name="desc"
+                        placeholder="desc"
+                        value={this.state.desc}
+                        onChange={this.inputFormHandler}
+                    ></textarea>
                     <input type="submit"></input>
                 </form>
             </div>
