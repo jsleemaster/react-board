dbdff7e926a5d1303c84a148b5ac4609773b6338
sunmyoung <roeowo123@naver.com>
2021-06-28 23:17:58 +0900

    state 끌어올리기

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,66 +2,77 @@ import React, { Component } from 'react';
 import Square from './Square';
 //게임 컨텐츠
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        }
-    }
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? "X" : "O";
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         squares: Array(9).fill(null),
+    //         xIsNext: true
+    //     }
+    // }
+
     renderSquare(i) {
-        return (<Square
-            value={this.state.squares[i]}
-            onClick={
-                () => this.handleClick(i)
-            }
-        ></Square >
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={
+                    () => this.props.onClick(i)
+                }
+            ></Square >
         );
     }
-
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-        // const status = "Next Player : " + (this.state.xIsNext ? "X" : "O");
         return (
             <div>
-                <div>
-                    <div className="status">{status}</div>
-                    <div className="board-row">
-                        {this.renderSquare(0)}
-                        {this.renderSquare(1)}
-                        {this.renderSquare(2)}
-                    </div>
-                    <div className="board-row">
-                        {this.renderSquare(3)}
-                        {this.renderSquare(4)}
-                        {this.renderSquare(5)}
-                    </div>
-                    <div className="board-row">
-                        {this.renderSquare(6)}
-                        {this.renderSquare(7)}
-                        {this.renderSquare(8)}
-                    </div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
-        )
+        );
     }
+    // render() {
+    // const winner = calculateWinner(this.state.squares);
+    // let status;
+    // if (winner) {
+    //     status = 'Winner: ' + winner;
+    // } else {
+    //     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // }
+    // // const status = "Next Player : " + (this.state.xIsNext ? "X" : "O");
+    // return (
+    //     <div>
+    //         <div>
+    //             <div className="status">{status}</div>
+    //             <div className="board-row">
+    //                 {this.renderSquare(0)}
+    //                 {this.renderSquare(1)}
+    //                 {this.renderSquare(2)}
+    //             </div>
+    //             <div className="board-row">
+    //                 {this.renderSquare(3)}
+    //                 {this.renderSquare(4)}
+    //                 {this.renderSquare(5)}
+    //             </div>
+    //             <div className="board-row">
+    //                 {this.renderSquare(6)}
+    //                 {this.renderSquare(7)}
+    //                 {this.renderSquare(8)}
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
+    // }
 }
 //우승자 정하기
 function calculateWinner(squares) {
@@ -83,4 +94,60 @@ function calculateWinner(squares) {
     }
     return null;
 }
-export default Board;
+//Game 컴포넌트
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
+    handleClick(i) {
+        // const squares = this.state.squares.slice();
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            // squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
