f446dcb13cf6125db1000150b6b9a30694ca5a34
sunmyoung <roeowo123@naver.com>
2021-06-29 20:40:07 +0900

    key값

--- src/components/Board.js
+++ src/components/Board.js
@@ -102,6 +102,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
@@ -122,10 +123,27 @@ class Game extends React.Component {
             xIsNext: !this.state.xIsNext,
         });
     }
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
     render() {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+        //map 함수 이용
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
