0b3fded6fad8b5c66257d6448b19409c62469773
jsleemaster <roeowo123@naver.com>
2021-07-06 23:15:20 +0900

    끝말잇기 함수형

--- src/webPack/WordReply.jsx
+++ src/webPack/WordReply.jsx
@@ -4,8 +4,38 @@ const { useState } = require('react/cjs/react.production.min');
 const { Component } = React;
 
 const WordReply = () => {
-    const [text, setState] = useState('Hello Webpack');
-    return <div>{text}</div>;
+    const [word, setWord] = useState('이순명');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        if (word.length - 1 === value) {
+            setResult("딩동댕");
+            setValue('');
+            setWord(value);
+        } else{
+            setResult('땡');
+            setValue('');
+        }
+        onRefInput.current.focus();
+    }
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    }
+    //ref 사용법
+    const onRefInput = React.useRef('');
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmit}>
+                <input ref={onRefInput} type='text' value={value} onChange={onChangeInput}/>
+                <button>입력</button>
+            </form>
+            <div>{result}</div>
+        </>);
 }
 
 module.exports = WordReply;
