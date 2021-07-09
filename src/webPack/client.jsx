a364f4d7af7aed51653c458fd00317f061ee3aa1
jsleemaster <roeowo123@naver.com>
2021-07-09 22:18:32 +0900

    map 함수 이용

--- src/webPack/client.jsx
+++ src/webPack/client.jsx
@@ -2,5 +2,6 @@ const React = require('react');
 const ReactDom = require('react-dom');
 //끝말잇기
 const WordReply = require('./WordReply');
-
-ReactDom.render(<WordReply />, document.querySelector('#root'));
+//야구게임
+const NumberBaseBall = require('./NumberBaseBall');
+ReactDom.render(<WordReply />,<NumberBaseBall/>, document.querySelector('#root'));
