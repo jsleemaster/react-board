5ed8f0a0e7916ebbe65a76390c6c3b274f523324
jsleemaster <roeowo123@naver.com>
2021-07-14 21:57:43 +0900

    야구게임

--- src/webPack/Try.jsx
+++ src/webPack/Try.jsx
@@ -2,7 +2,9 @@ import React ,{Component} from 'react';
 class Try extends Component {
     render(){
         return(
-            <li><b>{this.props.value.title}</b> : {this.props.value.taste} 인덱스 값 : {this.props.i}
+            <li>
+                <div>{this.props.tryInfo.try}</div>
+                <div>{this.props.tryInfo.result}</div>
             </li>
         )
     }
