ebe43c57848105f797195a6bc4cb0246b32ef6eb
jsleemaster <roeowo123@naver.com>
2021-07-23 23:29:19 +0900

    props는 불변성

--- src/webPack/Try.jsx
+++ src/webPack/Try.jsx
@@ -3,15 +3,16 @@ import { useState } from 'react/cjs/react.development';
 //hooks에서는 memo 로 대체 , shouldcomponent 없고, pureComponent도 없음.
 const Try = memo(({tryInfo})=>{
         //props는 불변 값이기 때문에 스테이트를 받아와서 스테이트를 변경해줘야한다
-        const {result, setResult} = useState(tryInfo.result);
-        const onClick = () => {
-            //변경 값
-            setResult('1');
-        }
+        // const {result, setResult} = useState(tryInfo.result);
+        // const onClick = () => {
+        //     //변경 값
+        //     setResult('1');
+        // }
         return(
             <li>
                 <div>{tryInfo.try}</div>
-                <div onClick={onClick}>{result}</div>
+                {/* <div onClick={onClick}>{result}</div> */}
+                <div>{tryInfo.result}</div>
             </li>
         )
 });
